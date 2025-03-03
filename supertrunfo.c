f72c20660f80d165fec50fc52c0ed2bb2dbe32a7
enzo819 <ennogabri@gmail.com>
2025-03-03 15:43:33 +0000

    commit

--- supertrunfo.c
+++ supertrunfo.c
@@ -1,1 +1,13 @@
+#include <stdio.h>
 
+int main(){
+printf("Carta 1:\n");
+char B;
+char string[] = "B03";
+char string[] = "Brasília";
+int População =  2817381;
+float área = 5787.784;
+float pib = 265.847;
+int NumeroDePontosTuristicos = 30;
+
+}

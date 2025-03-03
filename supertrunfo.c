48d86edae768b3f31d714301c601b105ee599c40
enzo819 <ennogabri@gmail.com>
2025-03-03 22:56:29 +0000

    Adicionei a carta 02

--- supertrunfo.c
+++ supertrunfo.c
@@ -6,19 +6,37 @@ char codigo[20] = "B03";
 char cidade[20] = "Brasília";
 int populacao = 2817068;
 double area = 5789.16;
-float pib = 265.847;
+float pib = 265.84;
 int Pontosturisticos = 30;
 
 printf("Carta 1:\n");
-printf("Estado : %c\n" , estado);
-printf("Código : %s\n" , codigo);
-printf("Cidade : %s\n" , cidade);
-printf("População : %d\n" , populacao);
-printf("Área em km2 : %f\n" , area);
-printf("Pib : %f\n" , pib);
-printf("Número de pontos turísticos : %d" , Pontosturisticos);
+printf("Estado: %c\n" , estado);
+printf("Código: %s\n" , codigo);
+printf("Cidade: %s\n" , cidade);
+printf("População: %d\n" , populacao);
+printf("Área em km2: %f\n" , area);
+printf("Pib: %f\n" , pib);
+printf("Número de pontos turísticos: %d\n" , Pontosturisticos);
+
+char estadO ='C';
+char codigO[20] = "C02";
+char cidadE[20] = "Cuiabá";
+int populacaO = 623000;
+double areA = 4327.448;
+double piB = 237.0000000;
+int pontosturisticoS = 16;
 
 
 
+printf("Carta 2:\n");
+printf("Estado: %c\n" , estadO);
+printf("Código: %s\n" , codigO);
+printf("Cidade: %s\n" , cidadE);
+printf("População: %d\n" , populacaO);
+printf("Área em Km2: %f\n" , areA);
+printf("Pib: %f\n" , piB);
+printf("Número de pontos turísticos: %d\n" , pontosturisticoS);
+
+return 0;
 
 }

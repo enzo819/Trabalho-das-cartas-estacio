7b38079b3b8a86ee4fe3915cbbb2a3e086501e90
enzo819 <ennogabri@gmail.com>
2025-03-03 15:46:14 +0000

    fiz o commit

--- supertrunfo.c
+++ supertrunfo.c
@@ -1,13 +1,24 @@
 #include <stdio.h>
 
 int main(){
-printf("Carta 1:\n");
-char B;
-char string[] = "B03";
-char string[] = "Brasília";
-int População =  2817381;
-float área = 5787.784;
+char estado = 'B';
+char codigo[20] = "B03";
+char cidade[20] = "Brasília";
+int populacao = 2817068;
+double area = 5789.16;
 float pib = 265.847;
-int NumeroDePontosTuristicos = 30;
+int Pontosturisticos = 30;
+
+printf("Carta 1:\n");
+printf("Estado : %c\n" , estado);
+printf("Código : %s\n" , codigo);
+printf("Cidade : %s\n" , cidade);
+printf("População : %d\n" , populacao);
+printf("Área em km2 : %f\n" , area);
+printf("Pib : %f\n" , pib);
+printf("Número de pontos turísticos : %d" , Pontosturisticos);
+
+
+
 
 }
